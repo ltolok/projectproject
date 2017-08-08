17aec70f831eca295c06c68c1362cfd7a91cdac7
mama <ltolok07@gmail.com>
2017-08-08 23:52:38 +0300

    08.08.17

--- src/lesson13/MiddleLine.java
+++ src/lesson13/MiddleLine.java
@@ -4,13 +4,12 @@ public class MiddleLine {
     static String s[] = new String[]{"String", "code", "Practice"};
 
     public static void main(String[] args) {
-        m(s);
+        middle(s);
     }
 
-    private static void m(String s[]) {
-        for (int i = 0;
-             i < s.length; i++) {
-            System.out.printf("  %1$s%2$s", s[i].charAt(s[i].length() / 2 - 1), s[i].charAt(s[i].length() / 2));
+    private static void middle(String s[]) {
+        for (String s1 : s) {
+            System.out.printf("  %1$s%2$s", s1.charAt(s1.length() / 2 - 1), s1.charAt(s1.length() / 2));
         }
     }
 }
