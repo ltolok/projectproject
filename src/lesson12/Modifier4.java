b007248870be3a2288250209065d0bcf2b5af356
mama <ltolok07@gmail.com>
2017-08-09 22:57:16 +0300

    09.08.17

---
+++ src/lesson12/Modifier4.java
@@ -0,0 +1,12 @@
+package lesson12;
+
+import lesson11.AccessModifier;
+
+public class Modifier4 {
+    public static void main(String s[]) {
+        //AccessModifier.m1(AccessModifier.s);
+        //AccessModifier.m1(AccessModifier.s1);
+        //AccessModifier.m1(AccessModifier.s2);
+        AccessModifier.m1(AccessModifier.s3);
+    }
+}
