8523dfca584f325a4643653c2d59c116d5ce5a44
mama <ltolok07@gmail.com>
2017-08-12 18:14:59 +0300

    12.08.17

---
+++ src/lesson14/Regxp3.java
@@ -0,0 +1,24 @@
+package lesson14;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regxp3 {
+    public static String strMinLengh;
+
+    public static void main(String[] args) {
+        String s = "Допустимые идентификаторы: MinTemp, sum, x4, $test, my_number.";
+        String regex = "[^:,;,;.,$,_,\\s]+";
+        int minLength = 999;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            if (minLength > matcher.group().length()) {
+                minLength = matcher.group().length();
+                strMinLengh = matcher.group();
+            }
+        }
+        System.out.println("В строке  " + "\"" + s + "\"");
+        System.out.println("первое слово, в котором число различных символов минимально " + "(" + minLength + ")" + " это  " + strMinLengh + ".");
+    }
+}
