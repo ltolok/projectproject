2e3b053970d1f004f6be72429de786904424d442
mama <ltolok07@gmail.com>
2017-08-22 22:00:55 +0300

    22.08.17

--- src/lesson11/Modifier1.java
+++ src/lesson11/Modifier1.java
@@ -2,7 +2,7 @@ package lesson11;
 
 public class Modifier1 extends AccessModifier {
     public static void main(String[] args) {
-        AccessModifier am = new AccessModifier();
+        AccessModifier am = new Modifier1();
         //am.m1(am.s1);
         am.m1(am.s2);
         am.m1(am.s3);
