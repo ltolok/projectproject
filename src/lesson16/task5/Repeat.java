9c1d43d27d6542aba0496cc268244835a608433f
mama <ltolok07@gmail.com>
2017-09-17 21:35:37 +0300

    17.09.17

---
+++ src/lesson16/task5/Repeat.java
@@ -0,0 +1,19 @@
+package lesson16.task5;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Repeat {
+    public static void main(String[] args) {
+        String s = "1, 2, 3, 4, 4, 5";
+        String regex = "\\d";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        Set<Integer> s1 = new LinkedHashSet<>();
+        while (matcher.find()) {
+            s1.add(new Integer(Integer.parseInt(matcher.group())));
+        }
+        System.out.println(s1);
+    }
+}
