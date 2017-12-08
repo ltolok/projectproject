1381589edb5f3738738d9bc4e8751eeeb1fd6eae
mama <ltolok07@gmail.com>
2017-12-08 20:26:49 +0300

    07.12.17

--- src/lesson9/Recursion.java
+++ src/lesson9/Recursion.java
@@ -2,18 +2,18 @@ package lesson9;
 
 public class Recursion {
     public static void main(String[] args) {
-        print(7, 1);
+        print(1, 7);
     }
 
     public static void print(int a, int b) {
         System.out.println(a);
         if (a > b) {
             if (a != b) {
-                print(a + 1, b);
+                print(a - 1, b);
             }
         } else {
             if (a < b) {
-                print(a - 1, b);
+                print(a + 1, b);
             }
         }
     }
