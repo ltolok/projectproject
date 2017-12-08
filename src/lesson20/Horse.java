226edfa3c7e3a5b1de1781970a1466b4b21a4fa3
mama <ltolok07@gmail.com>
2017-12-08 20:29:39 +0300

    07.12.17

--- src/lesson20/Horse.java
+++ src/lesson20/Horse.java
@@ -9,6 +9,36 @@ public class Horse extends Animal implements Serializable {
         this.halter = halter;
     }
 
+    public Halter getHalter() {
+        return halter;
+    }
+
+    @Override
+    public String toString() {
+        return "Horse{" +
+                "halter=" + halter +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Horse horse = (Horse) o;
+
+        return halter != null ? halter.equals(horse.halter) : horse.halter == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return halter != null ? halter.hashCode() : 0;
+    }
+
+    public void setHalter(Halter halter) {
+        this.halter = halter;
+    }
+
     public static void main(String[] args) {
         Halter halter = new Halter();
         Horse horse = new Horse(halter);
